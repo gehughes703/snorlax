c6df27f1197a817a79c5fc0bc06efc8a378ef9e4
hughesg703 <granth6@vt.edu>
2018-03-20 21:19:28 -0400

    wrote out Request.java

--- src/main/java/httpClient/Request.java
+++ src/main/java/httpClient/Request.java
@@ -1,5 +1,51 @@
 package httpClient;
 
+import org.json.JSONObject;
+
 public class Request {
 
+	private String url;
+	private JSONObject params;
+	private JSONObject headers;
+	private JSONObject body;
+	
+	public Request(String url) {
+		this.url = url;
+		params = null;
+		headers = null;
+		body = null;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public void setParams(JSONObject params) {
+		this.params = params;
+	}
+	
+	
+	public void setHeaders(JSONObject headers) {
+		this.headers = headers;
+	}
+	
+	public void setBody(JSONObject body) {
+		this.body = body;
+	}
+	
+	public String url() {
+		return url;
+	}
+
+	public JSONObject getParams() {
+		return params;
+	}
+	
+	public JSONObject getHeaders() {
+		return headers;
+	}
+	
+	public JSONObject getBody() {
+		return body;
+	}
 }
